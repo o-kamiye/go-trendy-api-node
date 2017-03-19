7bb4024ec6c2ada5db186462d51a4dae8e3dabea
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2017-03-20 01:39:16 +0300

    Implement user login and registration

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -12,7 +12,7 @@ exports.index = (req, res) => {
 exports.login = (req, res) => {
 	let username = req.body.username;
 	let password = req.body.password;
-	let loginDetails = { "username": username, "password": password };
+	let loginDetails = { "username": username };
 	User.findOne(loginDetails, (err, user) => {
 		if (err) res.status(500).send(err);
 		if (!user)
@@ -21,13 +21,21 @@ exports.login = (req, res) => {
 				message: 'Authentication failed. Invalid username or password.'
 			});
 		else {
-			let token = jwt.sign(user, req.app.get('secret'), {
-				expiresIn: '1440m' // 24 hours
-			});
-			res.json({
-				success: true,
-				message: 'Enjoy your token',
-				token: token
+			passwordHash(password).verifyAgainst(user.password, (err, verified) => {
+				if (err) res.status(500).send(err);
+				if (!verified) res.status(401).json({
+					success: false,
+					message: 'Authentication failed. Invalid username or password.'
+				});
+				else {
+					let token = jwt.sign(user, req.app.get('secret'), {
+						expiresIn: '1440h' // 24 hours
+					});
+					let userObject = user.toObject();
+					userObject.token = token;
+					delete userObject.password;
+					res.json(userObject);
+				}
 			});
 		}
 	});
@@ -37,30 +45,30 @@ exports.register = (req, res) => {
 	let email = req.body.email;
 	let username = req.body.username;
 	let password = req.body.password;
-	User.findOne({"username": username}, (err, user) => {
+	User.findOne({"email": email}, (err, user) => {
 		if (err) res.status(500).send(err);
 		if (user) res.status(409).json({
-			username: 'Username address exists'
+			email: 'Email address exists'
 		});
 		else
-			User.findOne({"email": email}, (err, user) => {
+			User.findOne({"username": username}, (err, user) => {
 				if (err) res.status(500).send(err);
 				if (user) res.status(409).json({
-					email: 'Email address exists'
+					username: 'Username exists'
 				});
 				else {
 					passwordHash(password).hash((err, hash) => {
 						if (err) res.status(500).send(err);
 						password = hash;
-					});
-					let newUser = new User({
-						email: email,
-						username: username,
-						password: password
-					});
-					newUser.save((err) => {
-						if (err) res.status(500).send(err);
-						res.json(newUser);
+						let newUser = new User({
+							email: email,
+							username: username,
+							password: password
+						});
+						newUser.save((err) => {
+							if (err) res.status(500).send(err);
+							res.json(newUser);
+						});
 					});
 				}
 			});
